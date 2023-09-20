cbec34b9ef705c438279f58108078ff63ead4bfd
Ishan-Kalpadith <80539944+Ishan-Kalpadith@users.noreply.github.com>
2023-09-20 12:47:24 +0530

    Added missed mapper to EditUserService.cs

--- CustomerDetailsManagementApplication/CustomerDetailsManagementApp/Services/EditUserService.cs
+++ CustomerDetailsManagementApplication/CustomerDetailsManagementApp/Services/EditUserService.cs
@@ -33,20 +33,7 @@ namespace CustomerDetailsManagementApp.Services
                     return (false, "User not found");
                 }
 
-                if (!string.IsNullOrEmpty(userUpdate.Name))
-                {
-                    user.Name = userUpdate.Name;
-                }
-
-                if (!string.IsNullOrEmpty(userUpdate.Email))
-                {
-                    user.Email = userUpdate.Email;
-                }
-
-                if (!string.IsNullOrEmpty(userUpdate.Phone))
-                {
-                    user.Phone = userUpdate.Phone;
-                }
+                _mapper.Map(userUpdate, user);
 
                 await _userRepository.UpdateUserAsync(user);
 
